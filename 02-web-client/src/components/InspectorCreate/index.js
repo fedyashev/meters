c4154fd7e53db741a8782381c7101fca4499ccf3
Andrei Fedyashev <fedjashev@gmail.com>
2019-02-06 16:18:50 +0300

    Add progress bars to InspectorCreate, InspectorInfo, InspectorList

--- 02-web-client/src/components/InspectorCreate/index.js
+++ 02-web-client/src/components/InspectorCreate/index.js
@@ -1,20 +1,35 @@
 import React, {Component} from 'react';
 
 import api from '../../lib/api';
-import GoBackLink from '../GoBackLink';
+import ProgressBar from '../ProgressBar';
+import NavBar from '../NavBar';
 
 class InspectorCreate extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: props.user,
+      isProcessing: false
+    }
+  }
+
   handleInspectorCreate = (login, password, name) => {
-    const token = this.props.user.token;
+    const token = this.state.user.token;
+    this.setState({...this.state, isProcessing: true});
     api.createInspector(token, login, password, name)
       .then(inspector => {
         if (inspector) {
-          this.props.history.goBack();
+          this.setState({...this.state, isProcessing: false}, () => {
+            this.props.history.goBack();
+          });
         }
       })
       .catch(({error}) => {
-        this.props.setAlert('warning', error.message);
+        this.setState({...this.state, isProcessing: false}, () => {
+          this.props.setAlert('warning', error.message);
+        });
       });
   }
 
@@ -33,43 +48,41 @@ class InspectorCreate extends Component {
     }
 
     return (
-      <div className="container">
-        <NavBar {...this.props}/>
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-            <div className="text-center font-weight-bold mb-5">
-                <h3>Добавить инспектора</h3>
+      <div>
+        <div className="container">
+          <NavBar {...this.props}/>
+          <div className="row justify-content-center">
+            <div className="col-12 col-sm-12 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+              <div className="text-center font-weight-bold mb-5">
+                  <h3>Добавить инспектора</h3>
+              </div>
+              <form>
+                <div className="form-group">
+                  <input className="form-control" type="text" placeholder="Логин" required autoFocus ref={r => login = r}/>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" type="password" placeholder="Пароль" required ref={r => password = r}/>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" type="password" placeholder="Подтвердить пароль" required ref={r => confirmPassword = r}/>
+                </div>
+                <div className="form-group">
+                  <input className="form-control" type="text" placeholder="Ф.И.О. инспектора" required ref={r => name = r}/>
+                </div>
+                {
+                  this.state.isProcessing ?
+                    <ProgressBar message={'Обработка...'} large={true}/> :
+                    <div className="form-group">
+                      <button className="btn btn-primary btn-block" onClick={onClickCreateInspector}>Создать</button>
+                    </div>
+                }
+              </form>
             </div>
-            <form>
-              <div className="form-group">
-                <input className="form-control" type="text" placeholder="Логин" required autoFocus ref={r => login = r}/>
-              </div>
-              <div className="form-group">
-                <input className="form-control" type="password" placeholder="Пароль" required ref={r => password = r}/>
-              </div>
-              <div className="form-group">
-                <input className="form-control" type="password" placeholder="Подтвердить пароль" required ref={r => confirmPassword = r}/>
-              </div>
-              <div className="form-group">
-                <input className="form-control" type="text" placeholder="Ф.И.О. инспектора" required ref={r => name = r}/>
-              </div>
-              <div className="form-group">
-                <button className="btn btn-primary btn-block" onClick={onClickCreateInspector}>Создать</button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
     );
   }
 };
-
-const NavBar = props => {
-  return (
-    <nav className="nav my-2">
-      <GoBackLink {...props}/>
-    </nav>
-  );
-}
 
 export default InspectorCreate;
