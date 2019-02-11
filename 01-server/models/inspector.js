9e2cd6c14104ef34af3b8ef2d6a91aa87df05306
Andrei Fedyashev <fedjashev@gmail.com>
2019-02-11 12:53:13 +0300

    Finish to test inspector model, fix inspector model, add to name field validation pattern

--- 01-server/models/inspector.js
+++ 01-server/models/inspector.js
@@ -8,6 +8,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
+        is: /^[a-zA-Zа-яА-Я][a-zA-Zа-яА-Я. ]*([a-zA-Zа-яА-Я]+|\.)$/,
         notEmpty: true,
         len: [2, 256],
         isString(value) {
@@ -23,7 +24,17 @@ module.exports = (sequelize, DataTypes) => {
     });
   Inspector.associate = function (models) {
     // associations can be defined here
-    Inspector.belongsTo(models.User);  // Inspector.UserId
+    Inspector.belongsTo(models.User, {
+      foreignKey: {
+        attribute: 'UserId',
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          isInt: true,
+          min: 1
+        }
+      }
+    });  // Inspector.UserId
   };
   return Inspector;
 };
--- 01-server/test/model-inspector.js
+++ 01-server/test/model-inspector.js
@@ -82,6 +82,95 @@ describe('Model Inspector', () => {
             assert.equal(inspector.name, testEntity.name);
         });
 
-        
+        it('Not create inspector with name contain @<>!?,[]{}\\|/%$#^:;&*~+-=', async () => {
+            const testEntity = { name: 'Иванов @<>!?,[]{}\\|/%$#^:;&*~+-=  И.И.', UserId: 1 };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+
+        it('Not create inspector with name contain whitespace at last character', async () => {
+            const testEntity = { name: 'Иванов И.И. ', UserId: 1 };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+
+        it('Not create inspector with name contain whitespace at first character', async () => {
+            const testEntity = { name: ' Иванов И.И.', UserId: 1 };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+    });
+
+    describe('UserId', () => {
+        it('Not create inspector with UserId is null', async () => {
+            const testEntity = { name: 'Иванов И.И.', UserId: null };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+
+        it('Not create inspector with UserId is string', async () => {
+            const testEntity = { name: 'Иванов И.И.', UserId: ' 1' };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+
+        it('Not create inspector with UserId is float', async () => {
+            const testEntity = { name: 'Иванов И.И.', UserId: 1.5 };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+
+        it('Not create inspector with UserId is zero', async () => {
+            const testEntity = { name: 'Иванов И.И.', UserId: 0 };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });
+
+        it('Not create inspector with UserId is negative integer number', async () => {
+            const testEntity = { name: 'Иванов И.И.', UserId: -1 };
+            let inspector = null;
+            try {
+                inspector = await Inspector.create({name: testEntity.name, UserId: testEntity.UserId});
+            } catch (error) {
+                
+            }
+            assert.typeOf(inspector, 'null');
+        });        
+
     });
 });
